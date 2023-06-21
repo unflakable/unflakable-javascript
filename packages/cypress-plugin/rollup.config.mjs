bc52139b657d9ffddc3a132ab3a3ed965c33e2e1
David A. Ramos <ramos@unflakable.com>
2023-06-21 16:07:17 -0700

    [cypress] Combine rollup config
    Rollup supports a config array, which allows us to invoke
    it just once. This also moves config-wrapper-sync.ts into the
    main build step, which allows it to share code with the rest of
    the CommonJS build, yielding a smaller bundle size.

--- packages/cypress-plugin/rollup.config.mjs
+++ packages/cypress-plugin/rollup.config.mjs
@@ -6,59 +6,84 @@ import pluginNodeResolve from "@rollup/plugin-node-resolve";
 import pluginTypescript from "@rollup/plugin-typescript";
 
 /**
+ * Bundle the internal @unflakable/plugins-common package, along with dependencies used by
+ * vendored Cypress code that Cypress itself also bundles (i.e., doesn't list in its public
+ * package.json as deps) in dist/, but leave most other imported packages as an external. Internal
+ * modules begin with `.` or `/`. We don't include `term-size` here because it depends on a
+ * bundled vendor/ directory that rollup doesn't include.
+ *
+ * @type {import("rollup").IsExternal}
+ */
+const isExternal = (id) =>
+  !id.startsWith(".") &&
+  !id.startsWith("/") &&
+  !id.startsWith("src/") &&
+  !id.startsWith("@unflakable/plugins-common/") &&
+  ![
+    // Avoid having skip-tests depend on @unflakable/js-api, which could pull in Node dependencies
+    // that Webpack v5 doesn't bundle by default. It's also unclear which versions of Webpack
+    // support sub-path exports from package.json like this. To avoid requiring any changes to
+    // the user's Webpack config, we try to make skip-tests self-contained and easy to import.
+    "@unflakable/js-api/consts",
+    "@unflakable/plugins-common",
+    "widest-line",
+  ].includes(id);
+
+const plugins = [
+  pluginCommonJs(),
+  pluginJson(),
+  pluginNodeResolve({ preferBuiltins: true }),
+  pluginTypescript({ tsconfig: "src/tsconfig.json" }),
+];
+
+const treeshake = {
+  // Assume internal modules do not have side effects when they're imported. This helps remove
+  // unnecessary require()'s from the transpiled code.
+  moduleSideEffects: (id, external) => external,
+};
+
+/**
  * @type {import("rollup").NormalizedInputOptions}
  */
-export default {
-  // NB: We exclude src/config-wrapper.ts since that needs to be compiled as an ESM target in
-  // order to be able to import user cypress.config.js files for projects that use ESM. The reason
-  // is that Cypress loads our config file and expects the default export to be the config
-  // object, which requires us to load the user config file when our script loads. Dynamic
-  // import() of ESM (require() can't import ESM modules) is async, but CommonJS doesn't support
-  // await at the top level of a file (ESM does). To summarize: (1) code that's imported by Cypress
-  // (with the exception of the config file, due to (2)) should be CommonJS, while (2) code that
-  // imports user code should be ESM so that it supports both CommonJS and ESM user code.
-  input: [
-    "src/index.ts",
-    "src/main.ts",
-    "src/reporter.ts",
-    "src/skip-tests.ts",
-  ],
-  output: {
-    format: "cjs",
-    /**
-     * @param {import("rollup").PreRenderedChunk} chunk
-     * @returns {string}
-     */
-    banner: (chunk) => (chunk.name === "main" ? "#!/usr/bin/env node" : ""),
+export default [
+  {
+    // NB: We exclude src/config-wrapper.ts since that needs to be compiled as an ESM target in
+    // order to be able to import user cypress.config.js files for projects that use ESM. The reason
+    // is that Cypress loads our config file and expects the default export to be the config
+    // object, which requires us to load the user config file when our script loads. Dynamic
+    // import() of ESM (require() can't import ESM modules) is async, but CommonJS doesn't support
+    // await at the top level of a file (ESM does). To summarize: (1) code that's imported by Cypress
+    // (with the exception of the config file, due to (2)) should be CommonJS, while (2) code that
+    // imports user code should be ESM so that it supports both CommonJS and ESM user code.
+    input: [
+      "src/config-wrapper-sync.ts",
+      "src/index.ts",
+      "src/main.ts",
+      "src/reporter.ts",
+      "src/skip-tests.ts",
+    ],
+    output: {
+      format: "cjs",
+      dir: "dist",
+      /**
+       * @param {import("rollup").PreRenderedChunk} chunk
+       * @returns {string}
+       */
+      banner: (chunk) => (chunk.name === "main" ? "#!/usr/bin/env node" : ""),
+    },
+    external: isExternal,
+    plugins,
+    treeshake,
   },
-  // Bundle the internal @unflakable/plugins-common package, along with dependencies used by
-  // vendored Cypress code that Cypress itself also bundles (i.e., doesn't list in its public
-  // package.json as deps) in dist/, but leave most other imported packages as an external. Internal
-  // modules begin with `.` or `/`. We don't include `term-size` here because it depends on a
-  // bundled vendor/ directory that rollup doesn't include.
-  external: (id) =>
-    !id.startsWith(".") &&
-    !id.startsWith("/") &&
-    !id.startsWith("src/") &&
-    !id.startsWith("@unflakable/plugins-common/") &&
-    ![
-      // Avoid having skip-tests depend on @unflakable/js-api, which could pull in Node dependencies
-      // that Webpack v5 doesn't bundle by default. It's also unclear which versions of Webpack
-      // support sub-path exports from package.json like this. To avoid requiring any changes to
-      // the user's Webpack config, we try to make skip-tests self-contained and easy to import.
-      "@unflakable/js-api/consts",
-      "@unflakable/plugins-common",
-      "widest-line",
-    ].includes(id),
-  plugins: [
-    pluginCommonJs(),
-    pluginJson(),
-    pluginNodeResolve({ preferBuiltins: true }),
-    pluginTypescript({ tsconfig: "src/tsconfig.json" }),
-  ],
-  treeshake: {
-    // Assume internal modules do not have side effects when they're imported. This helps remove
-    // unnecessary require()'s from the transpiled code.
-    moduleSideEffects: (id, external) => external,
+  {
+    input: "src/config-wrapper.ts",
+    output: {
+      chunkFileNames: "[name]-[hash].mjs",
+      file: "dist/config-wrapper.mjs",
+      format: "es",
+    },
+    external: isExternal,
+    plugins,
+    treeshake,
   },
-};
+];
